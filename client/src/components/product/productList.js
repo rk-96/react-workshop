1ec031f154d67a8bd2754be6604df14d1423bcb4
JK LAP <kicvong@gmail.com>
2019-09-22 23:38:08 +0700

    first commit

---
+++ client/src/components/product/productList.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import ProductItem from "./productitem";
+
+class ProductList extends Component {
+
+
+    showProducts() {
+        return this.props.products && this.props.products.map(product => (
+            <ProductItem key={product.productId} product={product} onAddOrder={this.props.onAddOrder} />
+        ))
+    }
+
+    render() {
+        return (
+            <div className="row">
+                {this.showProducts()}
+            </div>
+        );
+    }
+}
+
+export default ProductList;
